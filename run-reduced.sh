1b3b60d161ac093b6a7aa52612480508d89cd43e
tneuroth <tyneuroth@gmail.com>
2018-08-01 12:51:34 -0400

    ...

---
+++ run-reduced.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mpirun -np 4\
+    ./summarizer /home/tn0/xgc.mesh.bp \
+                 /home/tn0/xgc.bfield.bp \
+                 /home/tn0/ \
+                 /home/tn0/units.m \
+                 /media/tn0/data/datasets/ITER_SummaryMPI/ \
+                 /home/tn0/repos/xgc-summarizer/source/grid-algorithms/triangulation.obj
+
