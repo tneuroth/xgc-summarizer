9d47f64e543150318bce94d3a85c06870e9758e8
tneuroth <tyneuroth@gmail.com>
2018-09-07 08:26:07 -0400

    ...

--- source/Main.cpp
+++ source/Main.cpp
@@ -18,6 +18,9 @@ typedef float ValueType;
 
 int main( int argc, char** argv )
 {
+
+    std::cout << "summarizer started" << std::endl;
+    
     if( argc < 7 )
     {
         cerr << "expected:\
@@ -40,21 +43,23 @@ int main( int argc, char** argv )
     MPI_Comm mpiReaderComm;
 
     MPI_Init( &argc, &argv );
+
+    std::cout << "MPI initialized in summarizer" << std::endl;
+
     MPI_Comm_rank( MPI_COMM_WORLD, &wrank  );
     MPI_Comm_size( MPI_COMM_WORLD, &wnproc );
 
-    const unsigned int color = 99;
+    std::cout << "wrank=" << wrank << ", wnproc=" << wnproc << std::endl;
+
+    const unsigned int color = 12899;
     MPI_Comm_split( MPI_COMM_WORLD, color, wrank, &mpiReaderComm );
 
     MPI_Comm_rank( mpiReaderComm, &rank   );
     MPI_Comm_size( mpiReaderComm, &nRanks );
 
-    int err  = adios_read_init_method ( ADIOS_READ_METHOD_BP, mpiReaderComm, "verbose=3" );
+    std::cout << "rank=" << wrank << ", nRanks=" << wnproc << std::endl;
 
-    if( rank == 0 )
-    {
-        cout << "nRanks:\t" << nRanks << endl;
-    }
+    int err  = adios_read_init_method ( ADIOS_READ_METHOD_BP, mpiReaderComm, "verbose=3" );
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -67,6 +72,8 @@ int main( int argc, char** argv )
     const bool splitByBlocks             = std::string( argv[ 7 ] ) == "true";
     const bool inSitu                    = std::string( argv[ 8 ] ) == "true";
     const bool appendedReadMode          = std::string( argv[ 9 ] ) == "true";
+
+    std::cout << "before summarizer intialize" << std::endl;
 
     TN::XGCAggregator< ValueType > aggregator(
         adios2conf,
@@ -82,10 +89,12 @@ int main( int argc, char** argv )
     nRanks,
     mpiReaderComm );
 
-    if( argc == 11 )
-    {
-        aggregator.reduceMesh( argv[ 10 ] );
-    }
+    std::cout << "after summarizer intialize" << std::endl;
+
+    // if( argc == 11 )
+    // {
+    //     aggregator.reduceMesh( argv[ 10 ] );
+    // }
 
     if( rank == 0 )
     {
@@ -95,6 +104,7 @@ int main( int argc, char** argv )
     // finalize the adios 1 mesh reader
     adios_read_finalize_method ( ADIOS_READ_METHOD_BP );
 
+    std::cout << "running summarizer" << std::endl;
     aggregator.run();
 
     MPI_Finalize();
