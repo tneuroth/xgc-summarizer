438f1d8f6ac899682bf81de27bb1239ff4c73453
Tyson Neuroth <tneuroth@titan-ext4.ccs.ornl.gov>
2018-09-10 14:06:52 -0400

    reorganize

---
+++ module-scripts/module-to-load-titan.sh
@@ -0,0 +1,15 @@
+module unload PrgEnv-pgi PrgEnv-gnu PrgEnv-cray
+module load PrgEnv-gnu
+#module swap gcc gcc/6.3.0
+#module load cray-petsc/3.6.3.0
+module load cray-petsc/3.7.6.1
+module load cmake3/3.9.0
+module load craype-hugepages2M
+module load cudatoolkit
+module load python python_numpy
+
+module use -a /lustre/atlas/world-shared/csc143/jyc/titan/sw/modulefiles 
+module load adios2
+
+export XGC_PLATFORM=titan.gcc.jyc 
+
---
+++ run-scripts/local/run-is.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#arguments
+    #adios2 config file
+    #mesh file path
+    #bfield file path
+    #particle data directory or filepath if single particle file mode
+    #units.m file path
+    #output directory (where to write the summary)
+    #whether the mpi process reads a just it's chunk of the particles in the file
+    #whether to run the program in situ (waiting for steps to be written from simulation)
+    #whether there is one particle file (vs different files per time step)
+
+DATA_DIR=/media/tn0/data/datasets/inSituTest
+
+mpirun -np 8\
+    ./summarizer ${DATA_DIR}/adios2cfg.xml \
+                 ${DATA_DIR}/xgc.mesh.bp \
+                 ${DATA_DIR}/xgc.bfield.bp \
+                 ${DATA_DIR}/xgc.particle.bp \
+                 ${DATA_DIR}/units.m \
+                 ${DATA_DIR}/summary/ \
+                 true \
+                 true \
+                 true
+
