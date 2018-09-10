7b7f7a8c1dcb28e89b2bd59a7b58c04cd0d7f481
tneuroth <tyneuroth@gmail.com>
2018-09-10 15:06:41 -0400

    ...

--- source/Main.cpp
+++ source/Main.cpp
@@ -39,24 +39,24 @@ int main( int argc, char** argv )
     int wrank = 0, wnproc = 1;
     int rank  = 0, nRanks = 1;
 
-    MPI_Comm mpiReaderComm = MPI_COMM_WORLD;
+    MPI_Comm mpiReaderComm;
 
     MPI_Init( &argc, &argv );
 
     std::cout << "MPI initialized in summarizer" << std::endl;
 
-    // MPI_Comm_rank( MPI_COMM_WORLD, &wrank  );
-    // MPI_Comm_size( MPI_COMM_WORLD, &wnproc );
+    MPI_Comm_rank( MPI_COMM_WORLD, &wrank  );
+    MPI_Comm_size( MPI_COMM_WORLD, &wnproc );
 
-    // std::cout << "wrank=" << wrank << ", wnproc=" << wnproc << std::endl;
+    std::cout << "wrank=" << wrank << ", wnproc=" << wnproc << std::endl;
 
-    // const unsigned int color = 12899;
-    // MPI_Comm_split( MPI_COMM_WORLD, color, wrank, &mpiReaderComm );
+    const unsigned int color = 12899;
+    MPI_Comm_split( MPI_COMM_WORLD, color, wrank, &mpiReaderComm );
 
     MPI_Comm_rank( mpiReaderComm, &rank   );
     MPI_Comm_size( mpiReaderComm, &nRanks );
 
-    std::cout << "rank=" << wrank << ", nRanks=" << wnproc << std::endl;
+    std::cout << "rank=" << rank << ", nRanks=" << nproc << std::endl;
 
     int err  = adios_read_init_method ( ADIOS_READ_METHOD_BP, MPI_COMM_SELF, "verbose=3" );
 
